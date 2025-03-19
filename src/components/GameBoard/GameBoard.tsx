4c810a079c153a5bde4a9909e9cb93bd5377030d
cmdspeed <cmdspeed@gmail.com>
2025-03-19 20:29:05 +0100

    add animation board

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGameStore } from "../../store/GameStore";
 import { Card } from "./Card";
 import "../styles/GameBoard/GameBoard.scss";
@@ -18,8 +18,26 @@ export const GameBoard: React.FC = () => {
     setDifficulty(level);
   };
 
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isOpacity, setIsOpacity] = useState(false);
+  useEffect(() => {
+    setIsOpacity(true);
+    setIsAnimating(true);
+
+    const timer = setTimeout(() => {
+      setIsAnimating(false);
+    }, 800); // time animation (800ms)
+
+    // Czyszczenie timera
+    return () => clearTimeout(timer);
+  }, [selectedLevel]);
+
   return (
-    <div className="game-board">
+    <div
+      className={`game-board ${isOpacity ? "" : "opacity"}  ${
+        isAnimating ? "animating" : ""
+      }`}
+    >
       <MainButton onClick={resetGame} className="reset-button">
         Restart Game
       </MainButton>
